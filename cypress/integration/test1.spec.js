05caffbefd39f54455bcb05b49158968c1bc880a
Walmyr <wlsf82@gmail.com>
2021-12-11 01:31:08 +0100

    Add some explanatory comments on tests

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -1,5 +1,12 @@
 describe('Dependency', () => {
   beforeEach(() => {
+    /**
+     * Here, we're using a static fixture as the intercept's response.
+     * However, that is just to make a point.
+     * Let's supose the response comes from the real API, which would
+     * make the result dynamic. That is, at different times the request
+     * would return different data.
+     */
     cy.intercept('GET', '**/search?query=**', { fixture: 'stories' })
       .as('getStories')
     cy.visit('https://hackernews-seven.vercel.app/')
@@ -7,6 +14,11 @@ describe('Dependency', () => {
   })
 
   it('gets "dynamic" info, and saves into files', () => {
+    /**
+     * This is not really a test. There are no assertions on it.
+     * It is just getting same data from the first rendered anchor element,
+     * and saving them into text files, for later usage (by another test).
+     */
     cy.get('a')
       .first()
       .then($anchor => {
--- cypress/integration/test2.spec.js
+++ cypress/integration/test2.spec.js
@@ -1,3 +1,15 @@
+/**
+ * What would happen if this file happen to be executed before its dependency?
+ * Or, if its dependency fails?
+ *
+ * There are three things that come to my mind. It could:
+ *
+ * - fail due to not having a URL to visit yet defined
+ * - run based on outdated data (e.g., a fixture file created a while ago, most probably with outdated data)
+ * - or, with luck, run based on updated data
+ *
+ * The point is, this test depends on another, and this, on its own, is considered a bad practice.
+ */
 describe('Dependant', () => {
   beforeEach(() => {
     cy.fixture('url')
